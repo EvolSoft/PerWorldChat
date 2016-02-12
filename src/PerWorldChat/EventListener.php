ac75bc6bbc860f5cc6c264aabe639dfbf29216a0
Flavius12 <info@evolsoft.tk>
2016-02-12 19:28:27 +0100

    Fixed a crash with recipients

--- src/PerWorldChat/EventListener.php
+++ src/PerWorldChat/EventListener.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
- * PerWorldChat (v1.2) by EvolSoft
+ * PerWorldChat (v1.3) by EvolSoft
  * Developer: EvolSoft (Flavius12)
  * Website: http://www.evolsoft.tk
- * Date: 01/01/2015 05:32 PM (UTC)
- * Copyright & License: (C) 2014-2015 EvolSoft
+ * Date: 12/02/2016 06:27 PM (UTC)
+ * Copyright & License: (C) 2014-2016 EvolSoft
  * Licensed under MIT (https://github.com/EvolSoft/PerWorldChat/blob/master/LICENSE)
  */
 
@@ -28,18 +28,14 @@ class EventListener extends PluginBase implements Listener{
 		$player = $event->getPlayer();
 		$this->cfg = $this->plugin->getConfig()->getAll();
 		$recipients = $event->getRecipients();
-		for($i = 0; $i < count($recipients); $i++){
-			$levelplayers = $recipients[$i];
-			if($levelplayers instanceof Player){
-				if($player->getLevel() != $levelplayers->getLevel()){
-					$message[] = $i;
-					foreach($message as $messages){
-						unset($recipients[$i]);
-						$event->setRecipients(array_values($recipients));
-					}
+		foreach($recipients as $key => $recipient){
+			if($recipient instanceof Player){
+				if($recipient->getLevel() != $player->getLevel()){
+					unset($recipients[$key]);
 				}
 			}
 		}
+		$event->setRecipients($recipients);
 		//Checking Chat Disabled
 		if($this->plugin->isChatDisabled($player->getLevel()->getName())){
 		    //Check if log-chat-disabled is enabled
